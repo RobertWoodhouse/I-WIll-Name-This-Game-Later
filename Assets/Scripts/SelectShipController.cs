99817eb3776ee1894e888af948f8db47247d73ac
RobertWoodhouse <RMDWoodhouse@live.com>
2020-04-24 20:35:50 +0100

    Created Main Menu
    - Added UI for main menu
    - Ship Selection panel

---
+++ Assets/Scripts/SelectShipController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectShipController : MonoBehaviour
+{
+    public Text nameTxt, descriptionTxt;
+    public Image shipImg;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // TODO test functions
+        nameTxt.text = ChangeName();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private string ChangeName()
+    {
+        string[] names = { "", "S.S. BUC NASTY", "BANTON CHRONICLE" };
+
+        // TODO For method to change names when buttons is pressed
+        return names[2];
+    }
+}
