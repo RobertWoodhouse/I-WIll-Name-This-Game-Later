cff82f91bfcb6436a24823367c070223230cf2cf
RobertWoodhouse <RMDWoodhouse@live.com>
2020-04-25 10:13:48 +0100

    Added Ship Select
    - Ship Select controller created

--- Assets/Scripts/SelectShipController.cs
+++ Assets/Scripts/SelectShipController.cs
@@ -8,24 +8,44 @@ public class SelectShipController : MonoBehaviour
     public Text nameTxt, descriptionTxt;
     public Image shipImg;
 
-    // Start is called before the first frame update
+    [SerializeField]
+    private Sprite[] _shipSprites;
+    [SerializeField]
+    private Button _leftBtn, _rightBtn;
+    private int _selectNum = 0;
+    private string[] _names = { "SIR LENWORTH", "S.S. BUC NASTY", "BANTON CHRONICLE" };
+    private string[] _descriptions =
+    {
+        "SIR LENWORTH IS FAST \nUNPARALLELED SPEED",
+        "FROM THE US MILLITARY \nTHEY SAY THE SKY IS COVERED IN DARKNESS WHEN THIS SHIP RAINS GUNFIRE \nTHE S.S. BUC NASTY BOASTS SUPERIOR FIRE POWER",
+        "FRESH FROM THE HILLS OF JA \nTHE BANTON CHRONICLE \n\"DUPPY KNOW WHO FI FRIGHTEN\" \nMAXIMUM SPEED AND FIRE POWER"
+    };
+
     void Start()
     {
-        // TODO test functions
-        nameTxt.text = ChangeName();
+        SelectShip(0); // TODO save last ship selected on phone memory
+        _leftBtn.onClick.AddListener(LeftButtonOnClick);
+        _rightBtn.onClick.AddListener(RightButtonOnClick);
     }
 
-    // Update is called once per frame
-    void Update()
+    void LeftButtonOnClick()
     {
-        
+        _selectNum--;
+        if (_selectNum < 0) _selectNum = (_shipSprites.Length - 1);
+        SelectShip(_selectNum);
     }
 
-    private string ChangeName()
+    void RightButtonOnClick()
     {
-        string[] names = { "", "S.S. BUC NASTY", "BANTON CHRONICLE" };
+        _selectNum++;
+        if (_selectNum > (_shipSprites.Length - 1)) _selectNum = 0;
+        SelectShip(_selectNum);
+    }
 
-        // TODO For method to change names when buttons is pressed
-        return names[2];
+    void SelectShip(int select)
+    {
+        nameTxt.text = _names[select];
+        descriptionTxt.text = _descriptions[select];
+        shipImg.sprite = _shipSprites[select];
     }
 }
